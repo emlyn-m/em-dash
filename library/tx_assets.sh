83a7f3096aa589184416e2ab105dfd92c9bc2b94
Emlyn Matheson <emlyn@emlyn.xyz>
2026-02-07 15:53:11 +1100

    feat: add support for doc transfer to tx_assets

--- library/tx_assets.sh
+++ library/tx_assets.sh
@@ -10,9 +10,10 @@ gen_tx_cmd() {
     echo "echo '$(cat $1 | base64 -w 0)' | base64 -d > $2/$(basename $1) && "
 }
 
-HELP_MSG="\x1b[38;5;139m\x1b[1mHELP:\x1b[0m tx_assets.sh [data files] --fonts [font files]"
+HELP_MSG="\x1b[38;5;139m\x1b[1mHELP:\x1b[0m tx_assets.sh [data files] --docs [document files] --fonts [font files]"
+REMOTE_DATA_PATH="/"
+REMOTE_DOC_PATH="/mnt/us/documents"
 REMOTE_FONT_PATH="/usr/share/fonts"
-REMOTE_DATA_PATH="/data"
 TX_PORT=1337
 
 if [ "$#" -eq 0 ]; then
@@ -32,6 +33,14 @@ while [[ "$#" -gt 0 ]]; do
             else
                 echo -e "\x1b[38;5;139m\x1b[1mERR:\x1b[0m --fonts without any font files" >&2
                 exit 1
+            fi
+            ;;
+        --docs)
+            if [ -n "$2" ]; then
+                active_mode_name=" document"
+                active_mode_path=$REMOTE_DOC_PATH
+            else
+                echo -e "\x1b[38;5;139m\x1b[1mERR:\x1b[0m --docs without any doc files" >&2
             fi
             ;;
         *)
--- library/tx_file.sh
+++ library/tx_file.sh
@@ -2,14 +2,16 @@
 
 FPATH_LOCAL="/home/emlyn/pets/kindle/build_kindlepw2/dash"
 FPATH_REMOTE="/dash_test"
+FPATH_REMOTE_BOOKS="/mnt/us/documents"
 BOOK_NAME="dash"
 
 TX_PORT=1338
-STDOUT_IP=10.210.88.11
+STDOUT_IP=10.21.83.11
 STDOUT_PORT=1339
 
-book_content="\"#!/bin/sh\n# Name: ; run $BOOK_NAME\n# Author: winter\n\n rm /tmp/f; mkfifo /tmp/f; cat /tmp/f | $FPATH_REMOTE 2>&1 | nc $STDOUT_IP $STDOUT_PORT > /tmp/f \""
+book_content="\"#!/bin/sh\n# Name: ; run $BOOK_NAME\n# Author: winter\n\n rm /tmp/f; mkfifo /tmp/f; source /.env; cat /tmp/f | $FPATH_REMOTE 2>&1 | nc -w 15 $STDOUT_IP $STDOUT_PORT > /tmp/f \""
 echo -e "\x1b[38;5;139m\x1b[1mINFO:\x1b[0m ready to transmit on port $TX_PORT"
-echo "mount / -o remount,rw && rm -f $FPATH_REMOTE && rm -f /mnt/us/documents/$BOOK_NAME.sh && echo '$(cat $FPATH_LOCAL | base64 -w 0)' | base64 -d > $FPATH_REMOTE && chmod +x $FPATH_REMOTE && echo -e $book_content > /mnt/us/documents/$BOOK_NAME.sh && exit" | nc -lvp $TX_PORT 2>/dev/null
+echo "mount / -o remount,rw && rm -f $FPATH_REMOTE && rm -f $FPATH_REMOTE_BOOKS/$BOOK_NAME.sh && echo '$(cat $FPATH_LOCAL | base64 -w 0)' | base64 -d > $FPATH_REMOTE && chmod +x $FPATH_REMOTE && echo -e $book_content > $FPATH_REMOTE_BOOKS/$BOOK_NAME.sh && exit" | nc -lvp $TX_PORT 2>/dev/null
+
 echo -e "\n\x1b[38;5;139m\x1b[1mINFO:\x1b[0m listening for stdout/stderr on port $STDOUT_PORT"
 nc -lvp $STDOUT_PORT 2>/dev/null

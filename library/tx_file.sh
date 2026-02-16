767f7e4db64a0739b22dca599428f860ac3470aa
Emlyn Matheson <emlyn@emlyn.xyz>
2026-02-16 21:21:59 +1100

    dx: separate secret and config env files (less annoying after a backup)

--- library/tx_file.sh
+++ library/tx_file.sh
@@ -9,7 +9,7 @@ TX_PORT=1338
 STDOUT_IP=10.21.83.11
 STDOUT_PORT=1339
 
-book_content="\"#!/bin/sh\n# Name: ; run $BOOK_NAME\n# Author: winter\n\n rm /tmp/f; mkfifo /tmp/f; source /.env; cat /tmp/f | $FPATH_REMOTE 2>&1 | nc -w 15 $STDOUT_IP $STDOUT_PORT > /tmp/f \""
+book_content="\"#!/bin/sh\n# Name: ; run $BOOK_NAME\n# Author: winter\n\n rm /tmp/f; mkfifo /tmp/f; source /.env; source /.env.config; cat /tmp/f | $FPATH_REMOTE 2>&1 | nc -w 15 $STDOUT_IP $STDOUT_PORT > /tmp/f \""
 echo -e "\x1b[38;5;139m\x1b[1mINFO:\x1b[0m ready to transmit on port $TX_PORT"
 echo "mount / -o remount,rw && rm -f $FPATH_REMOTE && rm -f $FPATH_REMOTE_BOOKS/$BOOK_NAME.sh && echo '$(cat $FPATH_LOCAL | base64 -w 0)' | base64 -d > $FPATH_REMOTE && chmod +x $FPATH_REMOTE && echo -e $book_content > $FPATH_REMOTE_BOOKS/$BOOK_NAME.sh && exit" | nc -lvp $TX_PORT 2>/dev/null
 

